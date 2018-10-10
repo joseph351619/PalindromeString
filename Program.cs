5fe2b7669e968749d02d723cd4b4a92dd2ccd335
Joseph <sungfuchiu@gmail.com>
2018-10-10 20:49:27 +0800

    Palindrome integer

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int answer = 0;
+            while (true)
+            {
+                x = int.Parse(Console.ReadLine());
+                Console.WriteLine(IsPalindrome(x));
+            }
+
+        }
+
+        static bool IsPalindrome(int x)
+        {
+            int recordInput = x;
+            if(x < 0) return false;
+            if (x == 0) return true;
+            int comparedAnswer = 0;
+            while(x != 0)
+            {
+                comparedAnswer = x % 10 + comparedAnswer * 10;
+                x = x / 10;
+            }
+            if(recordInput == comparedAnswer)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
